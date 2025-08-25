17e0b707c4be7a624ded91ba9ca8559fc9b3b481
Iwo Plaza <iwoplaza@gmail.com>
2025-08-25 17:15:00 +0200

    Fix tsdown.config.ts

--- packages/degl/tsdown.config.ts
+++ packages/degl/tsdown.config.ts
@@ -2,7 +2,5 @@ import { defineConfig } from 'tsdown';
 import typegpu from 'unplugin-typegpu/rolldown';
 
 export default defineConfig({
-  entry: ['./src/webgl/index.ts', './src/webgl2/index.ts'],
-  dts: true,
   plugins: [typegpu({})],
 });
