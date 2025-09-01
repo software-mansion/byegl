4233e54294d29d0d6d721148ff3fb0a0cc99d5e9
Iwo Plaza <iwoplaza@gmail.com>
2025-09-01 16:38:30 +0200

    Basic usage docs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,10 +5,15 @@ import { defineConfig } from 'astro/config';
 import typegpu from 'unplugin-typegpu/vite';
 import mkcert from 'vite-plugin-mkcert';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   base: '/byegl',
+
   vite: {
     plugins: [tailwindcss(), typegpu({}), mkcert()],
   },
+
+  integrations: [mdx()],
 });
