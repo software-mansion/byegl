a2e1e85c99bec0a733f76acac4e75f7ab53ab230
Iwo Plaza <iwoplaza@gmail.com>
2026-03-09 13:03:11 +0100

    feat: Reporting created contexts if __BYEGL_INSPECT__ is defined (and createContext API)

--- packages/byegl/src/index.ts
+++ packages/byegl/src/index.ts
@@ -4,8 +4,18 @@ import { ByeGLContext } from './byegl-context.ts';
 import { ByeGLProgram } from './program.ts';
 import { $internal } from './types.ts';
 import { ShaderkitWGSLGenerator } from './wgsl/shaderkit-wgsl-generator.ts';
+import { addContext } from './globals.ts';
 
+export type { ByeGLContext } from './byegl-context.ts';
+
+let polyfillEnabled = false;
 export async function enable() {
+  if (polyfillEnabled) {
+    // No-op if already enabled.
+    return () => {};
+  }
+  polyfillEnabled = true;
+
   const originalGetContext = HTMLCanvasElement.prototype.getContext as any;
 
   // Doing everything asynchronous here, since WebGL is mostly synchronous.
@@ -18,15 +28,44 @@ export async function enable() {
   ) {
     if (contextId === 'webgl' || contextId === 'webgl2' || contextId === 'experimental-webgl') {
       const wgslGen = new ShaderkitWGSLGenerator();
-      return new ByeGLContext(contextId === 'webgl2' ? 2 : 1, root, this, wgslGen);
+      const ctx = new ByeGLContext(contextId === 'webgl2' ? 2 : 1, root, this, wgslGen);
+      addContext(ctx);
+      return ctx;
     }
 
     return originalGetContext!.call(this, contextId, ...args);
   };
 
   return () => {
+    polyfillEnabled = false;
     HTMLCanvasElement.prototype.getContext = originalGetContext;
   };
+}
+
+export interface ByeGLCreateContextOptions {
+  readonly canvas: HTMLCanvasElement;
+  readonly version: 1 | 2;
+}
+
+/**
+ * Creates a new ByeGL context from a canvas element.
+ * If you'd like this to happen automatically when the canvas is created,
+ * you can use the `enable` function instead.
+ *
+ * @example
+ * ```ts
+ * // instead of: const gl = canvas.getContext('webgl2');
+ * const gl = await byegl.createContext({ canvas, version: 2 });
+ * ```
+ */
+export async function createContext(options: ByeGLCreateContextOptions): Promise<ByeGLContext> {
+  // Doing everything asynchronous here, since WebGL is mostly synchronous.
+  const root = await tgpu.init();
+
+  const wgslGen = new ShaderkitWGSLGenerator();
+  const ctx = new ByeGLContext(options.version, root, options.canvas, wgslGen);
+  addContext(ctx);
+  return ctx;
 }
 
 export function isIntercepted(gl: WebGLRenderingContext | WebGL2RenderingContext): boolean {
