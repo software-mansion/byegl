7f0bc113871a72c3a6d9418d54f8376edab46059
Iwo Plaza <iwoplaza@gmail.com>
2026-03-10 10:10:38 +0100

    feat: Framebuffer support (#38)

--- packages/byegl/src/framebuffer.ts
+++ packages/byegl/src/framebuffer.ts
@@ -1,13 +1,41 @@
 import type { TgpuRoot } from 'typegpu';
+import type { ByeGLTexture } from './texture.ts';
 import { $internal } from './types.ts';
 
 /**
  * The internal state of byegl framebuffers
  */
 export class ByeGLFramebufferInternal {
-  constructor(_root: TgpuRoot) {}
+  readonly #root: TgpuRoot;
 
-  destroy() {}
+  colorAttachment: ByeGLTexture | null = null;
+
+  #depthTexture: GPUTexture | undefined;
+
+  constructor(root: TgpuRoot) {
+    this.#root = root;
+  }
+
+  getOrCreateDepthTexture(width: number, height: number): GPUTexture {
+    if (
+      !this.#depthTexture ||
+      this.#depthTexture.width !== width ||
+      this.#depthTexture.height !== height
+    ) {
+      this.#depthTexture?.destroy();
+      this.#depthTexture = this.#root.device.createTexture({
+        size: [width, height],
+        format: 'depth24plus',
+        usage: GPUTextureUsage.RENDER_ATTACHMENT,
+      });
+    }
+    return this.#depthTexture;
+  }
+
+  destroy() {
+    this.#depthTexture?.destroy();
+    this.#depthTexture = undefined;
+  }
 }
 
 export class ByeGLFramebuffer {
